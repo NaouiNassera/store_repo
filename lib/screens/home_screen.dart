b1a0299e17db1c35a26bfbbbb9617eb379ef1df1
Joseph <aiglon.eboue@gmail.com>
2024-03-26 16:01:21 +0100

    Home screen implementation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:store_repo/widgets/common/marquee_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            MarqueeWidget(),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://static.thcdn.com/widgets/262-en/52/original-FreeCap_1920x750_UK-085352.jpg'))),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.all(12),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: const Text(
+                      'Shop now !',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
